5d0a4b72f0f64d8e59b8d271a73eda30461a5e8d
donutwizard666 <thebeardedwarrior@icloud.com>
2020-07-15 19:20:47 -0700

    deleted all and created index to fetch from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-// import CovidData from './components/Card.js';
 import './App.css';
-import CovidData from './components/Card';
+
 
 function App() {
   return (
     <div className="App">
       COVID TRACKER APP
-      <CovidData />
     </div>
   );
 }
